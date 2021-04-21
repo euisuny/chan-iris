216095cef36839f3029d0899ceae245f9a41d49f
euisuny <ey222@cornell.edu>
2021-04-21 13:23:21 -0400

    Added message passing steps

--- lang.v
+++ lang.v
@@ -8,11 +8,11 @@ From iris.heap_lang Require Export locations.
 (******************************************************************)
 (** ** Syntax, machine state, and atomic reductions **)
 (******************************************************************)
-Definition chan : Set := nat.
+Definition chan_id := positive.
 Definition proph_id := positive.
 
 Inductive base_lit : Set :=
-  | LitChan (c : chan) | LitInt (n : Z) | LitBool (b : bool) | LitUnit | LitProphecy (p: proph_id).
+  | LitChan (c : chan_id) | LitInt (n : Z) | LitBool (b : bool) | LitUnit | LitProphecy (p: proph_id) | LitLoc (l : loc).
 
 Inductive expr :=
   (* Values *)
@@ -37,6 +37,13 @@ Inductive expr :=
   (* Prophecy *)
   | NewProph
   | Resolve (e0 : expr) (e1 : expr) (e2 : expr) (* wrapped expr, proph, val *)
+  (* Heap *)
+  | AllocN (e1 e2 : expr) (* array length (positive number), initial value *)
+  | Free (e : expr)
+  | Load (e : expr)
+  | Store (e1 : expr) (e2 : expr)
+  | CmpXchg (e0 : expr) (e1 : expr) (e2 : expr) (* Compare-exchange *)
+  | FAA (e1 : expr) (e2 : expr) (* Fetch-and-add *)
 with val :=
   | LitV (v : base_lit)
   | RecV (f x : binder) (e : expr)
@@ -57,10 +64,34 @@ Definition to_val (e : expr) : option val :=
   | _ => None
   end.
 
-(** The state: heaps of [option chan]s, with [None] representing deallocated locations. *)
-Record state : Type := {
-  heap: gmap loc (option chan);
-}.
+Definition lit_is_unboxed (l: base_lit) : Prop :=
+  match l with
+  (** Disallow comparing (erased) prophecies with (erased) prophecies, by
+  considering them boxed. *)
+  | LitProphecy _ => False
+  | LitInt _ | LitBool _  | LitLoc _ | LitUnit | LitChan _ => True
+  end.
+Definition val_is_unboxed (v : val) : Prop :=
+  match v with
+  | LitV l => lit_is_unboxed l
+  | InjLV (LitV l) => lit_is_unboxed l
+  | InjRV (LitV l) => lit_is_unboxed l
+  | _ => False
+  end.
+
+Global Instance lit_is_unboxed_dec l : Decision (lit_is_unboxed l).
+Proof. destruct l; simpl; exact (decide _). Defined.
+Global Instance val_is_unboxed_dec v : Decision (val_is_unboxed v).
+Proof. destruct v as [ | | | [] | [] ]; simpl; exact (decide _). Defined.
+
+(** We just compare the word-sized representation of two values, without looking
+into boxed data.  This works out fine if at least one of the to-be-compared
+values is unboxed (exploiting the fact that an unboxed and a boxed value can
+never be equal because these are disjoint sets). *)
+Definition vals_compare_safe (vl v1 : val) : Prop :=
+  val_is_unboxed vl ∨ val_is_unboxed v1.
+Global Arguments vals_compare_safe !_ !_ /.
+
 
 (** Equality and other typeclass stuff *)
 Lemma to_of_val v : to_val (of_val v) = Some v.
@@ -101,6 +132,18 @@ Proof.
      | NewProph, NewProph => left _
      | Resolve e0 e1 e2, Resolve e0' e1' e2' =>
         cast_if_and3 (decide (e0 = e0')) (decide (e1 = e1')) (decide (e2 = e2'))
+     | AllocN e1 e2, AllocN e1' e2' =>
+        cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+     | Free e, Free e' =>
+        cast_if (decide (e = e'))
+     | Load e, Load e' => cast_if (decide (e = e'))
+     | Store e1 e2, Store e1' e2' =>
+        cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+     | CmpXchg e0 e1 e2, CmpXchg e0' e1' e2' =>
+        cast_if_and3 (decide (e0 = e0')) (decide (e1 = e1')) (decide (e2 = e2'))
+     | FAA e1 e2, FAA e1' e2' =>
+        cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+
      | _, _ => right _
      end
    with gov (v1 v2 : val) {struct v1} : Decision (v1 = v2) :=
@@ -120,8 +163,117 @@ Defined.
 Global Instance val_eq_dec : EqDecision val.
 Proof. solve_decision. Defined.
 
+Global Instance base_lit_countable : Countable base_lit.
+Proof.
+  refine (inj_countable' (λ l, match l with
+  | LitInt n => (inl (inl n), None)
+  | LitBool b => (inl (inr b), None)
+  | LitUnit => (inr (inl ()), None)
+  | LitLoc l => (inr (inr l), None)
+  | LitProphecy p => (inl (inr true), Some p)
+  | LitChan c => (inl (inr false), Some c)
+  end) (λ l, match l with
+  | (inl (inl n), None) => LitInt n
+  | (inl (inr b), None) => LitBool b
+  | (inr (inl ()), None) => LitUnit
+  | (inl (inr false), Some c) => LitChan c
+  | (inr (inr l), None) => LitLoc l
+  | (_, Some p) => LitProphecy p
+  end) _). intros []; intuition. destruct b; eauto.
+Qed.
+Global Instance expr_countable : Countable expr.
+Proof.
+ set (enc :=
+   fix go e :=
+     match e with
+     | Val v => GenNode 0 [gov v]
+     | Var x => GenLeaf (inl (inl x))
+     | Rec f x e => GenNode 1 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); go e]
+     | App e1 e2 => GenNode 2 [go e1; go e2]
+     | Pair e1 e2 => GenNode 6 [go e1; go e2]
+     | Fst e => GenNode 7 [go e]
+     | Snd e => GenNode 8 [go e]
+     | InjL e => GenNode 9 [go e]
+     | InjR e => GenNode 10 [go e]
+     | Case e0 e1 e2 => GenNode 11 [go e0; go e1; go e2]
+     | Fork e => GenNode 12 [go e]
+     | AllocN e1 e2 => GenNode 13 [go e1; go e2]
+     | Free e => GenNode 14 [go e]
+     | Load e => GenNode 15 [go e]
+     | Store e1 e2 => GenNode 16 [go e1; go e2]
+     | CmpXchg e0 e1 e2 => GenNode 17 [go e0; go e1; go e2]
+     | FAA e1 e2 => GenNode 18 [go e1; go e2]
+     | NewProph => GenNode 19 []
+     | Resolve e0 e1 e2 => GenNode 20 [go e0; go e1; go e2]
+     | NewCh => GenNode 21 []
+     | Send e1 e2 => GenNode 22 [go e1; go e2]
+     | TryRecv e => GenNode 23 [go e]
+     end
+   with gov v :=
+     match v with
+     | LitV l => GenLeaf (inr (inl (B := ()) l))
+     | RecV f x e =>
+        GenNode 0 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); go e]
+     | PairV v1 v2 => GenNode 1 [gov v1; gov v2]
+     | InjLV v => GenNode 2 [gov v]
+     | InjRV v => GenNode 3 [gov v]
+     end
+   for go).
+ set (dec :=
+   fix go e :=
+     match e with
+     | GenNode 0 [v] => Val (gov v)
+     | GenLeaf (inl (inl x)) => Var x
+     | GenNode 1 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); e] => Rec f x (go e)
+     | GenNode 2 [e1; e2] => App (go e1) (go e2)
+     | GenNode 6 [e1; e2] => Pair (go e1) (go e2)
+     | GenNode 7 [e] => Fst (go e)
+     | GenNode 8 [e] => Snd (go e)
+     | GenNode 9 [e] => InjL (go e)
+     | GenNode 10 [e] => InjR (go e)
+     | GenNode 11 [e0; e1; e2] => Case (go e0) (go e1) (go e2)
+     | GenNode 12 [e] => Fork (go e)
+     | GenNode 13 [e1; e2] => AllocN (go e1) (go e2)
+     | GenNode 14 [e] => Free (go e)
+     | GenNode 15 [e] => Load (go e)
+     | GenNode 16 [e1; e2] => Store (go e1) (go e2)
+     | GenNode 17 [e0; e1; e2] => CmpXchg (go e0) (go e1) (go e2)
+     | GenNode 18 [e1; e2] => FAA (go e1) (go e2)
+     | GenNode 19 [] => NewProph
+     | GenNode 20 [e0; e1; e2] => Resolve (go e0) (go e1) (go e2)
+     | GenNode 21 [] => NewCh
+     | GenNode 22 [e1; e2] => Send (go e1) (go e2)
+     | GenNode 23 [e] => TryRecv (go e)
+     | _ => Val $ LitV LitUnit (* dummy *)
+     end
+   with gov (v : gen_tree (string + binder + (base_lit + ()))) :=
+     match v with
+     | GenLeaf (inr (inl l)) => LitV l
+     | GenNode 0 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); e] => RecV f x (go e)
+     | GenNode 1 [v1; v2] => PairV (gov v1) (gov v2)
+     | GenNode 2 [v] => InjLV (gov v)
+     | GenNode 3 [v] => InjRV (gov v)
+     | _ => LitV LitUnit (* dummy *)
+     end
+   for go).
+ refine (inj_countable' enc dec _).
+ refine (fix go (e : expr) {struct e} := _ with gov (v : val) {struct v} := _ for go).
+ - destruct e as [v| | | | | | | | | | | | | | | | | | | | |]; simpl; f_equal;
+     [exact (gov v)|done..].
+ - destruct v; by f_equal.
+Qed.
+Global Instance val_countable : Countable val.
+Proof. refine (inj_countable of_val to_val _); auto using to_of_val. Qed.
+
+
+(** The state: heaps of [option chan]s, with [None] representing deallocated locations. *)
+Record state : Type := {
+  heap: gmap loc (option val);
+  chan: gmap chan_id (option (gset val));
+  used_proph_id: gset proph_id;
+}.
 Global Instance state_inhabited : Inhabited state :=
-  populate {| heap := inhabitant |}.
+  populate {| heap := inhabitant; chan := inhabitant; used_proph_id := inhabitant |}.
 Global Instance val_inhabited : Inhabited val := populate (LitV LitUnit).
 Global Instance expr_inhabited : Inhabited expr := populate (Val inhabitant).
 
@@ -144,7 +296,22 @@ Inductive ectx_item :=
   | SendLCtx (e : expr)
   | SendRCtx (e : expr)
   | TryRecvCtx
-  | ForkCtx.
+  | ForkCtx
+  | AllocNLCtx (v2 : val)
+  | AllocNRCtx (e1 : expr)
+  | FreeCtx
+  | LoadCtx
+  | StoreLCtx (v2 : val)
+  | StoreRCtx (e1 : expr)
+  | CmpXchgLCtx (v1 : val) (v2 : val)
+  | CmpXchgMCtx (e0 : expr) (v2 : val)
+  | CmpXchgRCtx (e0 : expr) (e1 : expr)
+  | FaaLCtx (v2 : val)
+  | FaaRCtx (e1 : expr)
+  | ResolveLCtx (ctx : ectx_item) (v1 : val) (v2 : val)
+  | ResolveMCtx (e0 : expr) (v2 : val)
+  | ResolveRCtx (e0 : expr) (e1 : expr).
+
 
 Fixpoint fill_item (Ki : ectx_item) (e : expr) : expr :=
   match Ki with
@@ -161,6 +328,20 @@ Fixpoint fill_item (Ki : ectx_item) (e : expr) : expr :=
   | SendRCtx e1 => Send e e1
   | TryRecvCtx => TryRecv e
   | ForkCtx => Fork e
+  | AllocNLCtx v2 => AllocN e (Val v2)
+  | AllocNRCtx e1 => AllocN e1 e
+  | FreeCtx => Free e
+  | LoadCtx => Load e
+  | StoreLCtx v2 => Store e (Val v2)
+  | StoreRCtx e1 => Store e1 e
+  | CmpXchgLCtx v1 v2 => CmpXchg e (Val v1) (Val v2)
+  | CmpXchgMCtx e0 v2 => CmpXchg e0 e (Val v2)
+  | CmpXchgRCtx e0 e1 => CmpXchg e0 e1 e
+  | FaaLCtx v2 => FAA e (Val v2)
+  | FaaRCtx e1 => FAA e1 e
+  | ResolveLCtx K v1 v2 => Resolve (fill_item K e) (Val v1) (Val v2)
+  | ResolveMCtx ex v2 => Resolve ex e (Val v2)
+  | ResolveRCtx ex e1 => Resolve ex e1 e
   end.
 
 (** Substitution *)
@@ -180,6 +361,12 @@ Fixpoint subst (x : string) (v : val) (e : expr)  : expr :=
   | Fork e => Fork (subst x v e)
   | Send e1 e2 => Send (subst x v e1) (subst x v e2)
   | TryRecv e => TryRecv (subst x v e)
+  | AllocN e1 e2 => AllocN (subst x v e1) (subst x v e2)
+  | Free e => Free (subst x v e)
+  | Load e => Load (subst x v e)
+  | Store e1 e2 => Store (subst x v e1) (subst x v e2)
+  | CmpXchg e0 e1 e2 => CmpXchg (subst x v e0) (subst x v e1) (subst x v e2)
+  | FAA e1 e2 => FAA (subst x v e1) (subst x v e2)
   | Resolve ex e1 e2 => Resolve (subst x v ex) (subst x v e1) (subst x v e2)
   end.
 
@@ -187,11 +374,19 @@ Definition subst' (mx : binder) (v : val) : expr → expr :=
   match mx with BNamed x => subst x v | BAnon => id end.
 
 (** The stepping relation *)
-Definition state_upd_heap (f: gmap loc (option chan) → gmap loc (option chan)) (σ: state) : state :=
-  {| heap := f σ.(heap) |}.
+Definition state_upd_heap (f: gmap loc (option val) → gmap loc (option val)) (σ: state) : state :=
+  {| heap := f σ.(heap); used_proph_id := σ.(used_proph_id); chan := σ.(chan)|}.
 Global Arguments state_upd_heap _ !_ /.
 
-Fixpoint heap_array (l : loc) (vs : list chan) : gmap loc (option chan) :=
+Definition state_upd_used_proph_id (f: gset proph_id → gset proph_id) (σ: state) : state :=
+  {| heap := σ.(heap); used_proph_id := f σ.(used_proph_id); chan := σ.(chan)|}.
+Global Arguments state_upd_used_proph_id _ !_ /.
+
+Definition state_upd_chan (f: gmap chan_id (option (gset val)) → gmap chan_id (option (gset val))) (σ: state) : state :=
+  {| heap := σ.(heap); used_proph_id := σ.(used_proph_id); chan := f σ.(chan)|}.
+Global Arguments state_upd_chan !_ /.
+
+Fixpoint heap_array (l : loc) (vs : list val) : gmap loc (option val) :=
   match vs with
   | [] => ∅
   | v :: vs' => {[l := Some v]} ∪ heap_array (l +ₗ 1) vs'
@@ -221,7 +416,7 @@ Proof.
     auto with lia.
 Qed.
 
-Lemma heap_array_map_disjoint (h : gmap loc (option chan)) (l : loc) (vs : list chan) :
+Lemma heap_array_map_disjoint (h : gmap loc (option val)) (l : loc) (vs : list val) :
   (∀ i, (0 ≤ i)%Z → (i < length vs)%Z → h !! (l +ₗ i) = None) →
   (heap_array l vs) ##ₘ h.
 Proof.
@@ -231,7 +426,7 @@ Proof.
 Qed.
 
 (* [h] is added on the right here to make [state_init_heap_singleton] true. *)
-Definition state_init_heap (l : loc) (n : Z) (v : chan) (σ : state) : state :=
+Definition state_init_heap (l : loc) (n : Z) (v : val) (σ : state) : state :=
   state_upd_heap (λ h, heap_array l (replicate (Z.to_nat n) v) ∪ h) σ.
 
 Lemma state_init_heap_singleton l v σ :
@@ -240,6 +435,9 @@ Proof.
   destruct σ as [h p]. rewrite /state_init_heap /=. f_equiv.
   rewrite right_id insert_union_singleton_l. done.
 Qed.
+
+Notation None_ := (InjL (Val $ LitV $ LitUnit)).
+Notation "'Some_'  e" := (InjR (Val $ e)) (at level 10).
 
 Inductive head_step : expr → state → list observation → expr → state → list expr → Prop :=
   | RecS f x e σ :
@@ -262,8 +460,77 @@ Inductive head_step : expr → state → list observation → expr → state →
   | CaseRS v e1 e2 σ :
      head_step (Case (Val $ InjRV v) e1 e2) σ [] (App e2 (Val v)) σ []
   | ForkS e σ:
-      head_step (Fork e) σ [] (Val $ LitV LitUnit) σ [e].
-(* TODO : Add [head_step] for channel items *)
+      head_step (Fork e) σ [] (Val $ LitV LitUnit) σ [e]
+  | AllocNS n v σ l :
+     (0 < n)%Z →
+     (∀ i, (0 ≤ i)%Z → (i < n)%Z → σ.(heap) !! (l +ₗ i) = None) →
+     head_step (AllocN (Val $ LitV $ LitInt n) (Val v)) σ
+               []
+               (Val $ LitV $ LitLoc l) (state_init_heap l n v σ)
+               []
+  | FreeS l v σ :
+     σ.(heap) !! l = Some $ Some v →
+     head_step (Free (Val $ LitV $ LitLoc l)) σ
+               []
+               (Val $ LitV LitUnit) (state_upd_heap <[l:=None]> σ)
+               []
+  | LoadS l v σ :
+     σ.(heap) !! l = Some $ Some v →
+     head_step (Load (Val $ LitV $ LitLoc l)) σ [] (of_val v) σ []
+  | StoreS l v w σ :
+     σ.(heap) !! l = Some $ Some v →
+     head_step (Store (Val $ LitV $ LitLoc l) (Val w)) σ
+               []
+               (Val $ LitV LitUnit) (state_upd_heap <[l:=Some w]> σ)
+               []
+  | CmpXchgS l v1 v2 vl σ b :
+     σ.(heap) !! l = Some $ Some vl →
+     (* Crucially, this compares the same way as [EqOp]! *)
+     vals_compare_safe vl v1 →
+     b = bool_decide (vl = v1) →
+     head_step (CmpXchg (Val $ LitV $ LitLoc l) (Val v1) (Val v2)) σ
+               []
+               (Val $ PairV vl (LitV $ LitBool b)) (if b then state_upd_heap <[l:=Some v2]> σ else σ)
+               []
+  | FaaS l i1 i2 σ :
+     σ.(heap) !! l = Some $ Some (LitV (LitInt i1)) →
+     head_step (FAA (Val $ LitV $ LitLoc l) (Val $ LitV $ LitInt i2)) σ
+               []
+               (Val $ LitV $ LitInt i1) (state_upd_heap <[l:=Some $ LitV (LitInt (i1 + i2))]>σ)
+               []
+  | NewProphS σ p :
+     p ∉ σ.(used_proph_id) →
+     head_step NewProph σ
+               []
+               (Val $ LitV $ LitProphecy p) (state_upd_used_proph_id ({[ p ]} ∪.) σ)
+               []
+  | ResolveS p v e σ w σ' κs ts :
+     head_step e σ κs (Val v) σ' ts →
+     head_step (Resolve e (Val $ LitV $ LitProphecy p) (Val w)) σ
+               (κs ++ [(p, (v, w))]) (Val v) σ' ts
+  (* Message-passing *)
+  | NewChS σ c:
+      σ.(chan) !! c = Some $ None ->
+      head_step NewCh σ
+                []
+                (Val $ LitV $ LitChan c) (state_upd_chan <[c := Some $ empty]> σ)
+                []
+  | SendS σ M c v:
+      σ.(chan) !! c = Some $ Some $ M ->
+      head_step (Send (Val $ LitV $ LitChan c) (Val v)) σ
+                []
+                (Val $ LitV LitUnit) (state_upd_chan <[c := Some $ M ∪ {[ v ]}]> σ)
+                []
+  | TryRecvNoneS σ c:
+      σ.(chan) !! c = Some $ Some $ empty ->
+      head_step (TryRecv (Val $ LitV $ LitChan c)) σ
+                [] None_ σ []
+  | TryRecvSomeS σ c M v:
+      σ.(chan) !! c = Some $ Some $ (M ∪ {[ v ]}) ->
+      head_step (TryRecv (Val $ LitV $ LitChan c)) σ
+                []
+                (Some_ v) (state_upd_chan <[c := Some M]> σ)
+                [].
 
 (** Basic properties about the language *)
 Global Instance fill_item_inj Ki : Inj (=) (=) (fill_item Ki).
