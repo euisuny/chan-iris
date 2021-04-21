0e531a814823e681c2b5f5f371a670c3f00d49b4
euisuny <ey222@cornell.edu>
2021-04-21 11:10:58 -0400

    Add basic ChanLa

---
+++ lang.v
@@ -0,0 +1,368 @@
+From stdpp Require Export binders strings.
+From stdpp Require Import gmap.
+From iris.algebra Require Export ofe.
+From iris.program_logic Require Export language ectx_language ectxi_language.
+From iris.heap_lang Require Export locations.
+
+(* ChanLang *)
+(******************************************************************)
+(** ** Syntax, machine state, and atomic reductions **)
+(******************************************************************)
+Definition chan : Set := nat.
+Definition proph_id := positive.
+
+Inductive base_lit : Set :=
+  | LitChan (c : chan) | LitInt (n : Z) | LitBool (b : bool) | LitUnit | LitProphecy (p: proph_id).
+
+Inductive expr :=
+  (* Values *)
+  | Val (v : val)
+  (* Base lambda calculus *)
+  | Var (x : string)
+  | Rec (f x : binder) (e : expr)
+  | App (e1 e2 : expr)
+  (* Products *)
+  | Pair (e1 e2 : expr)
+  | Fst (e : expr)
+  | Snd (e : expr)
+  (* Sums *)
+  | InjL (e : expr)
+  | InjR (e : expr)
+  | Case (e0 : expr) (e1 : expr) (e2 : expr)
+  (* Message-passing *)
+  | NewCh
+  | Send (e1 e2 : expr)
+  | TryRecv (e : expr)
+  | Fork (e : expr)
+  (* Prophecy *)
+  | NewProph
+  | Resolve (e0 : expr) (e1 : expr) (e2 : expr) (* wrapped expr, proph, val *)
+with val :=
+  | LitV (v : base_lit)
+  | RecV (f x : binder) (e : expr)
+  | PairV (v1 v2 : val)
+  | InjLV (v : val)
+  | InjRV (v : val).
+
+Bind Scope expr_scope with expr.
+Bind Scope val_scope with val.
+
+Definition observation : Set := proph_id * (val * val).
+
+Notation of_val := Val (only parsing).
+
+Definition to_val (e : expr) : option val :=
+  match e with
+  | Val v => Some v
+  | _ => None
+  end.
+
+(** The state: heaps of [option chan]s, with [None] representing deallocated locations. *)
+Record state : Type := {
+  heap: gmap loc (option chan);
+}.
+
+(** Equality and other typeclass stuff *)
+Lemma to_of_val v : to_val (of_val v) = Some v.
+Proof. by destruct v. Qed.
+
+Lemma of_to_val e v : to_val e = Some v → of_val v = e.
+Proof. destruct e=>//=. by intros [= <-]. Qed.
+
+Global Instance of_val_inj : Inj (=) (=) of_val.
+Proof. intros ??. congruence. Qed.
+
+
+Global Instance base_lit_eq_dec : EqDecision base_lit.
+Proof. solve_decision. Defined.
+
+Global Instance expr_eq_dec : EqDecision expr.
+Proof.
+  refine (
+   fix go (e1 e2 : expr) {struct e1} : Decision (e1 = e2) :=
+     match e1, e2 with
+     | Val v, Val v' => cast_if (decide (v = v'))
+     | Var x, Var x' => cast_if (decide (x = x'))
+     | Rec f x e, Rec f' x' e' =>
+        cast_if_and3 (decide (f = f')) (decide (x = x')) (decide (e = e'))
+     | App e1 e2, App e1' e2' => cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+     | Pair e1 e2, Pair e1' e2' =>
+        cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+     | Fst e, Fst e' => cast_if (decide (e = e'))
+     | Snd e, Snd e' => cast_if (decide (e = e'))
+     | InjL e, InjL e' => cast_if (decide (e = e'))
+     | InjR e, InjR e' => cast_if (decide (e = e'))
+     | Case e0 e1 e2, Case e0' e1' e2' =>
+        cast_if_and3 (decide (e0 = e0')) (decide (e1 = e1')) (decide (e2 = e2'))
+     | Fork e, Fork e' => cast_if (decide (e = e'))
+     | Send e1 e2, Send e1' e2' => cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+     | TryRecv e, TryRecv e' => cast_if (decide (e = e'))
+     | NewCh, NewCh => left _
+     | NewProph, NewProph => left _
+     | Resolve e0 e1 e2, Resolve e0' e1' e2' =>
+        cast_if_and3 (decide (e0 = e0')) (decide (e1 = e1')) (decide (e2 = e2'))
+     | _, _ => right _
+     end
+   with gov (v1 v2 : val) {struct v1} : Decision (v1 = v2) :=
+     match v1, v2 with
+     | LitV e, LitV e' => cast_if (decide (e = e'))
+     | RecV f x e, RecV f' x' e' =>
+        cast_if_and3 (decide (f = f')) (decide (x = x')) (decide (e = e'))
+     | PairV e1 e2, PairV e1' e2' =>
+        cast_if_and (decide (e1 = e1')) (decide (e2 = e2'))
+     | InjLV e, InjLV e' => cast_if (decide (e = e'))
+     | InjRV e, InjRV e' => cast_if (decide (e = e'))
+     | _, _ => right _
+     end
+       for go); try (clear go gov; abstract intuition congruence).
+Defined.
+
+Global Instance val_eq_dec : EqDecision val.
+Proof. solve_decision. Defined.
+
+Global Instance state_inhabited : Inhabited state :=
+  populate {| heap := inhabitant |}.
+Global Instance val_inhabited : Inhabited val := populate (LitV LitUnit).
+Global Instance expr_inhabited : Inhabited expr := populate (Val inhabitant).
+
+Canonical Structure stateO := leibnizO state.
+Canonical Structure locO := leibnizO loc.
+Canonical Structure valO := leibnizO val.
+Canonical Structure exprO := leibnizO expr.
+
+(** Evaluation contexts *)
+Inductive ectx_item :=
+  | AppLCtx (v2 : val)
+  | AppRCtx (e1 : expr)
+  | PairLCtx (v2 : val)
+  | PairRCtx (e1 : expr)
+  | FstCtx
+  | SndCtx
+  | InjLCtx
+  | InjRCtx
+  | CaseCtx (e1 : expr) (e2 : expr)
+  | SendLCtx (e : expr)
+  | SendRCtx (e : expr)
+  | TryRecvCtx
+  | ForkCtx.
+
+Fixpoint fill_item (Ki : ectx_item) (e : expr) : expr :=
+  match Ki with
+  | AppLCtx v2 => App e (of_val v2)
+  | AppRCtx e1 => App e1 e
+  | PairLCtx v2 => Pair e (Val v2)
+  | PairRCtx e1 => Pair e1 e
+  | FstCtx => Fst e
+  | SndCtx => Snd e
+  | InjLCtx => InjL e
+  | InjRCtx => InjR e
+  | CaseCtx e1 e2 => Case e e1 e2
+  | SendLCtx e1 => Send e1 e
+  | SendRCtx e1 => Send e e1
+  | TryRecvCtx => TryRecv e
+  | ForkCtx => Fork e
+  end.
+
+(** Substitution *)
+Fixpoint subst (x : string) (v : val) (e : expr)  : expr :=
+  match e with
+  | Val _ | NewCh | NewProph => e
+  | Var y => if decide (x = y) then Val v else Var y
+  | Rec f y e =>
+     Rec f y $ if decide (BNamed x ≠ f ∧ BNamed x ≠ y) then subst x v e else e
+  | App e1 e2 => App (subst x v e1) (subst x v e2)
+  | Pair e1 e2 => Pair (subst x v e1) (subst x v e2)
+  | Fst e => Fst (subst x v e)
+  | Snd e => Snd (subst x v e)
+  | InjL e => InjL (subst x v e)
+  | InjR e => InjR (subst x v e)
+  | Case e0 e1 e2 => Case (subst x v e0) (subst x v e1) (subst x v e2)
+  | Fork e => Fork (subst x v e)
+  | Send e1 e2 => Send (subst x v e1) (subst x v e2)
+  | TryRecv e => TryRecv (subst x v e)
+  | Resolve ex e1 e2 => Resolve (subst x v ex) (subst x v e1) (subst x v e2)
+  end.
+
+Definition subst' (mx : binder) (v : val) : expr → expr :=
+  match mx with BNamed x => subst x v | BAnon => id end.
+
+(** The stepping relation *)
+Definition state_upd_heap (f: gmap loc (option chan) → gmap loc (option chan)) (σ: state) : state :=
+  {| heap := f σ.(heap) |}.
+Global Arguments state_upd_heap _ !_ /.
+
+Fixpoint heap_array (l : loc) (vs : list chan) : gmap loc (option chan) :=
+  match vs with
+  | [] => ∅
+  | v :: vs' => {[l := Some v]} ∪ heap_array (l +ₗ 1) vs'
+  end.
+
+Lemma heap_array_singleton l v : heap_array l [v] = {[l := Some v]}.
+Proof. by rewrite /heap_array right_id. Qed.
+
+Lemma heap_array_lookup l vs ow k :
+  heap_array l vs !! k = Some ow ↔
+  ∃ j w, (0 ≤ j)%Z ∧ k = l +ₗ j ∧ ow = Some w ∧ vs !! (Z.to_nat j) = Some w.
+Proof.
+  revert k l; induction vs as [|v' vs IH]=> l' l /=.
+  { rewrite lookup_empty. naive_solver lia. }
+  rewrite -insert_union_singleton_l lookup_insert_Some IH. split.
+  - intros [[-> ?] | (Hl & j & w & ? & -> & -> & ?)].
+    { eexists 0, _. rewrite loc_add_0. naive_solver lia. }
+    eexists (1 + j)%Z, _. rewrite loc_add_assoc !Z.add_1_l Z2Nat.inj_succ; auto with lia.
+  - intros (j & w & ? & -> & -> & Hil). destruct (decide (j = 0)); simplify_eq/=.
+    { rewrite loc_add_0; eauto. }
+    right. split.
+    { rewrite -{1}(loc_add_0 l). intros ?%(inj (loc_add _)); lia. }
+    assert (Z.to_nat j = S (Z.to_nat (j - 1))) as Hj.
+    { rewrite -Z2Nat.inj_succ; last lia. f_equal; lia. }
+    rewrite Hj /= in Hil.
+    eexists (j - 1)%Z, _. rewrite loc_add_assoc Z.add_sub_assoc Z.add_simpl_l.
+    auto with lia.
+Qed.
+
+Lemma heap_array_map_disjoint (h : gmap loc (option chan)) (l : loc) (vs : list chan) :
+  (∀ i, (0 ≤ i)%Z → (i < length vs)%Z → h !! (l +ₗ i) = None) →
+  (heap_array l vs) ##ₘ h.
+Proof.
+  intros Hdisj. apply map_disjoint_spec=> l' v1 v2.
+  intros (j&w&?&->&?&Hj%lookup_lt_Some%inj_lt)%heap_array_lookup.
+  move: Hj. rewrite Z2Nat.id // => ?. by rewrite Hdisj.
+Qed.
+
+(* [h] is added on the right here to make [state_init_heap_singleton] true. *)
+Definition state_init_heap (l : loc) (n : Z) (v : chan) (σ : state) : state :=
+  state_upd_heap (λ h, heap_array l (replicate (Z.to_nat n) v) ∪ h) σ.
+
+Lemma state_init_heap_singleton l v σ :
+  state_init_heap l 1 v σ = state_upd_heap <[l:=Some v]> σ.
+Proof.
+  destruct σ as [h p]. rewrite /state_init_heap /=. f_equiv.
+  rewrite right_id insert_union_singleton_l. done.
+Qed.
+
+Inductive head_step : expr → state → list observation → expr → state → list expr → Prop :=
+  | RecS f x e σ :
+     head_step (Rec f x e) σ [] (Val $ RecV f x e) σ []
+  | PairS v1 v2 σ :
+     head_step (Pair (Val v1) (Val v2)) σ [] (Val $ PairV v1 v2) σ []
+  | InjLS v σ :
+     head_step (InjL $ Val v) σ [] (Val $ InjLV v) σ []
+  | InjRS v σ :
+     head_step (InjR $ Val v) σ [] (Val $ InjRV v) σ []
+  | BetaS f x e1 v2 e' σ :
+     e' = subst' x v2 (subst' f (RecV f x e1) e1) →
+     head_step (App (Val $ RecV f x e1) (Val v2)) σ [] e' σ []
+  | FstS v1 v2 σ :
+     head_step (Fst (Val $ PairV v1 v2)) σ [] (Val v1) σ []
+  | SndS v1 v2 σ :
+     head_step (Snd (Val $ PairV v1 v2)) σ [] (Val v2) σ []
+  | CaseLS v e1 e2 σ :
+     head_step (Case (Val $ InjLV v) e1 e2) σ [] (App e1 (Val v)) σ []
+  | CaseRS v e1 e2 σ :
+     head_step (Case (Val $ InjRV v) e1 e2) σ [] (App e2 (Val v)) σ []
+  | ForkS e σ:
+      head_step (Fork e) σ [] (Val $ LitV LitUnit) σ [e].
+(* TODO : Add [head_step] for channel items *)
+
+(** Basic properties about the language *)
+Global Instance fill_item_inj Ki : Inj (=) (=) (fill_item Ki).
+Proof. induction Ki; intros ???; simplify_eq/=; auto with f_equal. Qed.
+
+Lemma fill_item_val Ki e :
+  is_Some (to_val (fill_item Ki e)) → is_Some (to_val e).
+Proof. intros [v ?]. induction Ki; simplify_option_eq; eauto. Qed.
+
+Lemma val_head_stuck e1 σ1 κ e2 σ2 efs : head_step e1 σ1 κ e2 σ2 efs → to_val e1 = None.
+Proof. destruct 1; naive_solver. Qed.
+
+Lemma head_ctx_step_val Ki e σ1 κ e2 σ2 efs :
+  head_step (fill_item Ki e) σ1 κ e2 σ2 efs → is_Some (to_val e).
+Proof. revert κ e2. induction Ki; inversion_clear 1; simplify_option_eq; eauto. Qed.
+
+Lemma fill_item_no_val_inj Ki1 Ki2 e1 e2 :
+  to_val e1 = None → to_val e2 = None →
+  fill_item Ki1 e1 = fill_item Ki2 e2 → Ki1 = Ki2.
+Proof.
+  revert Ki1. induction Ki2; intros Ki1; induction Ki1; naive_solver eauto with f_equal.
+Qed.
+
+Lemma alloc_fresh v n σ :
+  let l := fresh_locs (dom (gset loc) σ.(heap)) in
+  (0 < n)%Z →
+  head_step (AllocN ((Val $ LitV $ LitInt $ n)) (Val v)) σ []
+            (Val $ LitV $ LitLoc l) (state_init_heap l n v σ) [].
+Proof.
+  intros.
+  apply AllocNS; first done.
+  intros. apply (not_elem_of_dom (D := gset loc)).
+  by apply fresh_locs_fresh.
+Qed.
+
+Lemma new_proph_id_fresh σ :
+  let p := fresh σ.(used_proph_id) in
+  head_step NewProph σ [] (Val $ LitV $ LitProphecy p) (state_upd_used_proph_id ({[ p ]} ∪.) σ) [].
+Proof. constructor. apply is_fresh. Qed.
+
+Lemma heap_lang_mixin : EctxiLanguageMixin of_val to_val fill_item head_step.
+Proof.
+  split; apply _ || eauto using to_of_val, of_to_val, val_head_stuck,
+    fill_item_val, fill_item_no_val_inj, head_ctx_step_val.
+Qed.
+End heap_lang.
+
+(** Language *)
+Canonical Structure heap_ectxi_lang := EctxiLanguage heap_lang.heap_lang_mixin.
+Canonical Structure heap_ectx_lang := EctxLanguageOfEctxi heap_ectxi_lang.
+Canonical Structure heap_lang := LanguageOfEctx heap_ectx_lang.
+
+(* Prefer heap_lang names over ectx_language names. *)
+Export heap_lang.
+
+(** The following lemma is not provable using the axioms of [ectxi_language].
+The proof requires a case analysis over context items ([destruct i] on the
+last line), which in all cases yields a non-value. To prove this lemma for
+[ectxi_language] in general, we would require that a term of the form
+[fill_item i e] is never a value. *)
+Lemma to_val_fill_some K e v : to_val (fill K e) = Some v → K = [] ∧ e = Val v.
+Proof.
+  intro H. destruct K as [|Ki K]; first by apply of_to_val in H. exfalso.
+  assert (to_val e ≠ None) as He.
+  { intro A. by rewrite fill_not_val in H. }
+  assert (∃ w, e = Val w) as [w ->].
+  { destruct e; try done; eauto. }
+  assert (to_val (fill (Ki :: K) (Val w)) = None).
+  { destruct Ki; simpl; apply fill_not_val; done. }
+  by simplify_eq.
+Qed.
+
+Lemma prim_step_to_val_is_head_step e σ1 κs w σ2 efs :
+  prim_step e σ1 κs (Val w) σ2 efs → head_step e σ1 κs (Val w) σ2 efs.
+Proof.
+  intro H. destruct H as [K e1 e2 H1 H2].
+  assert (to_val (fill K e2) = Some w) as H3; first by rewrite -H2.
+  apply to_val_fill_some in H3 as [-> ->]. subst e. done.
+Qed.
+
+(** If [e1] makes a head step to a value under some state [σ1] then any head
+ step from [e1] under any other state [σ1'] must necessarily be to a value. *)
+Lemma head_step_to_val e1 σ1 κ e2 σ2 efs σ1' κ' e2' σ2' efs' :
+  head_step e1 σ1 κ e2 σ2 efs →
+  head_step e1 σ1' κ' e2' σ2' efs' → is_Some (to_val e2) → is_Some (to_val e2').
+Proof. destruct 1; inversion 1; naive_solver. Qed.
+
+Lemma irreducible_resolve e v1 v2 σ :
+  irreducible e σ → irreducible (Resolve e (Val v1) (Val v2)) σ.
+Proof.
+  intros H κs ? σ' efs [Ks e1' e2' Hfill -> step]. simpl in *.
+  induction Ks as [|K Ks _] using rev_ind; simpl in Hfill.
+  - subst e1'. inversion step. eapply H. by apply head_prim_step.
+  - rewrite fill_app /= in Hfill.
+    destruct K; (inversion Hfill; subst; clear Hfill; try
+      match goal with | H : Val ?v = fill Ks ?e |- _ =>
+        (assert (to_val (fill Ks e) = Some v) as HEq by rewrite -H //);
+        apply to_val_fill_some in HEq; destruct HEq as [-> ->]; inversion step
+      end).
+    eapply (H κs (fill_item _ (foldl (flip fill_item) e2' Ks)) σ' efs).
+    eapply (Ectx_step (Ks ++ [_])); last done; simpl; by rewrite fill_app.
+Qed.
