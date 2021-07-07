d4b2b1b16238cf9496a005e3a7ddaf585438385f
euisuny <ey222@cornell.edu>
2021-07-07 11:25:12 +0200

    Atomic spec for send

--- localchan.v
+++ localchan.v
@@ -1,15 +1,14 @@
 From iris.proofmode Require Import coq_tactics reduction.
 From iris.proofmode Require Export tactics.
-From iris.program_logic Require Import atomic.
+From iris.program_logic Require Import atomic weakestpre.
 From iris.prelude Require Import options.
 From iris.proofmode Require Export tactics.
 (* IY: What's the difference between [atomic] at [iris.program_logic]
  and [bi.lib]? *)
 From iris.bi Require Import atomic derived_laws interface.
 
-
 From chanlang Require Import
-     lang notation network_ra tactics primitive_laws.
+     class_instances lang notation network_ra tactics primitive_laws.
 Set Default Proof Using "Type".
 
 (* To start off, let's try defining a blocking receive. *)
@@ -44,25 +43,8 @@ Section atomic_invariants.
   Global Instance send_atomic s c m : Atomic s (chan_lang.Send (Val c) (Val m)).
   Proof. solve_atomic. Qed.
 
-  Lemma tac_wp_send
-    Δ Δ' s E i K l v v' vl vl' Φ :
-    v ∈ vl ->
-    v' ∈ vl' ->
-    MaybeIntoLaterNEnvs 1 Δ Δ' →
-    envs_lookup i Δ' = Some (false, l ↦ vl)%I →
-    match envs_simple_replace i false (Esnoc Enil i (l ↦ vl')) Δ' with
-    | Some Δ'' => envs_entails Δ'' (WP fill K (Val $ LitV LitUnit) @ s; E {{ Φ }})
-    | None => False
-    end ->
-    envs_entails Δ (WP fill K (chan_lang.Send (LitV $ LitLoc l) (Val v')) @ s; E {{ Φ }}).
-  Proof.
-    rewrite envs_entails_eq=> ???. intros.
-    destruct (envs_simple_replace _ _ _) as [Δ''|] eqn:HΔ''; [ | contradiction ].
-    rewrite -wp_bind. eapply bi.wand_apply; first by eapply wp_send.
-    rewrite into_laterN_env_sound envs_simple_replace_sound //; simpl.
-    rewrite right_id.
-  Admitted.
 
+Notation "'val'" := chan_lang.val.
   Ltac reshape_expr e tac :=
     let rec go K vs e :=
       match e with
@@ -76,10 +58,12 @@ Section atomic_invariants.
       | InjL ?e                         => add_item InjLCtx vs K e
       | InjR ?e                         => add_item InjRCtx vs K e
       | Case ?e0 ?e1 ?e2                => add_item (CaseCtx e1 e2) vs K e0
+      | Send ?e (Val ?v)                => add_item (SendLCtx v) vs K e
+      | Send ?e1 ?e2                    => add_item (SendRCtx e1) vs K e2
       end
     with add_item Ki vs K e :=
       lazymatch vs with
-      | []               => go (Ki :: K) (@nil (val * val)) e
+      | _               => go (Ki :: K) (@nil (val * val)) e
       end
     in
     go (@nil ectx_item) (@nil (val * val)) e.
@@ -101,31 +85,115 @@ Section atomic_invariants.
     | _ => fail "wp_store: not a 'wp'"
     end.
 
-  (* TODO: [v = ()] in conclusion. *)
+  Lemma tac_wp_expr_eval `{!chanG Σ} Δ s E Φ e e' :
+    (∀ (e'':=e'), e = e'') →
+    envs_entails Δ (WP e' @ s; E {{ Φ }}) → envs_entails Δ (WP e @ s; E {{ Φ }}).
+  Proof. by intros ->. Qed.
+  Lemma tac_twp_expr_eval `{!chanG Σ} Δ s E Φ e e' :
+    (∀ (e'':=e'), e = e'') →
+    envs_entails Δ (WP e' @ s; E [{ Φ }]) → envs_entails Δ (WP e @ s; E [{ Φ }]).
+  Proof. by intros ->. Qed.
+
+  Tactic Notation "wp_expr_eval" tactic3(t) :=
+    iStartProof;
+    lazymatch goal with
+    | |- envs_entails _ (wp ?s ?E ?e ?Q) =>
+      notypeclasses refine (tac_wp_expr_eval _ _ _ _ e _ _ _);
+        [let x := fresh in intros x; t; unfold x; notypeclasses refine eq_refl|]
+    | |- envs_entails _ (twp ?s ?E ?e ?Q) =>
+      notypeclasses refine (tac_twp_expr_eval _ _ _ _ e _ _ _);
+        [let x := fresh in intros x; t; unfold x; notypeclasses refine eq_refl|]
+    | _ => fail "wp_expr_eval: not a 'wp'"
+    end.
+  Ltac wp_expr_simpl := wp_expr_eval simpl.
+
+  Lemma tac_wp_value_nofupd `{!chanG Σ} Δ s E Φ v :
+    envs_entails Δ (Φ v) → envs_entails Δ (WP (Val v) @ s; E {{ Φ }}).
+  Proof. rewrite envs_entails_eq=> ->. by apply wp_value. Qed.
+  Lemma tac_twp_value_nofupd `{!chanG Σ} Δ s E Φ v :
+    envs_entails Δ (Φ v) → envs_entails Δ (WP (Val v) @ s; E [{ Φ }]).
+  Proof. rewrite envs_entails_eq=> ->. by apply twp_value. Qed.
+
+  Lemma tac_wp_pure `{!chanG Σ} Δ Δ' s E K e1 e2 φ n Φ :
+    PureExec φ n e1 e2 →
+    φ →
+    MaybeIntoLaterNEnvs n Δ Δ' →
+    envs_entails Δ' (WP (fill K e2) @ s; E {{ Φ }}) →
+    envs_entails Δ (WP (fill K e1) @ s; E {{ Φ }}).
+  Proof.
+    rewrite envs_entails_eq=> ??? HΔ'. rewrite into_laterN_env_sound /=.
+    (* We want [pure_exec_fill] to be available to TC search locally. *)
+    pose proof @pure_exec_fill.
+    rewrite HΔ' -lifting.wp_pure_step_later //.
+  Qed.
+  Lemma tac_twp_pure `{!chanG Σ} Δ s E K e1 e2 φ n Φ :
+    PureExec φ n e1 e2 →
+    φ →
+    envs_entails Δ (WP (fill K e2) @ s; E [{ Φ }]) →
+    envs_entails Δ (WP (fill K e1) @ s; E [{ Φ }]).
+  Proof.
+    rewrite envs_entails_eq=> ?? ->.
+    (* We want [pure_exec_fill] to be available to TC search locally. *)
+    pose proof @pure_exec_fill.
+    rewrite -total_lifting.twp_pure_step //.
+  Qed.
+
+ From iris Require Import bi.interface.
+ Import uPred.
+
+  Lemma tac_wp_send
+    Δ Δ' s E i K l m vl Φ :
+    MaybeIntoLaterNEnvs 1 Δ Δ' →
+    envs_lookup i Δ' = Some (false, l ↦ vl)%I →
+    match envs_simple_replace i false (Esnoc Enil i (l ↦ (vl ∪ {[m]}))) Δ' with
+    | Some Δ'' => envs_entails Δ'' (WP fill K (Val $ #()) @ s; E {{ Φ }})
+    | None => False
+    end ->
+    envs_entails Δ (WP fill K (chan_lang.Send (LitV $ LitLoc l) (Val m)) @ s; E {{ Φ }}).
+  Proof.
+    rewrite envs_entails_eq=> ???. intros.
+    destruct (envs_simple_replace _ _ _) as [Δ''|] eqn:HΔ''; [ | contradiction ].
+    rewrite -wp_bind. eapply wand_apply; first by eapply wp_send.
+    rewrite into_laterN_env_sound -later_sep envs_simple_replace_sound //; simpl.
+    rewrite right_id. apply later_mono, sep_mono_r, wand_mono; eauto.
+  Qed.
+
+  Ltac wp_value_head :=
+    lazymatch goal with
+    | |- envs_entails _ (wp ?s ?E (Val _) (λ _, fupd ?E _ _)) =>
+        eapply tac_wp_value_nofupd
+    | |- envs_entails _ (wp ?s ?E (Val _) (λ _, wp _ ?E _ _)) =>
+        eapply tac_wp_value_nofupd
+    (* | |- envs_entails _ (wp ?s ?E (Val _) _) => *)
+    (*     eapply tac_wp_value *)
+    | |- envs_entails _ (twp ?s ?E (Val _) (λ _, fupd ?E _ _)) =>
+        eapply tac_twp_value_nofupd
+    | |- envs_entails _ (twp ?s ?E (Val _) (λ _, twp _ ?E _ _)) =>
+        eapply tac_twp_value_nofupd
+    (* | |- envs_entails _ (twp ?s ?E (Val _) _) => *)
+    (*     eapply tac_twp_value *)
+    end.
+
+  Ltac wp_finish :=
+    wp_expr_simpl;
+    try wp_value_head;
+    pm_prettify.
+
   Lemma awp_send (c : loc) (M : gset chan_lang.val) (m : chan_lang.val) :
-    ⊢ <<< ∀ v, c ↦ M >>> send(c, m) @ ⊤ <<< c ↦ (M ∪ {[m]}), RET v >>>.
+    ⊢ <<< c ↦ M >>> send(c, m) @ ⊤ <<< c ↦ (M ∪ {[m]}), RET #() >>>.
   Proof.
     iIntros (Φ) "AU".
-    iMod "AU" as (v) "[H↦ [_ Hclose]]".
-    (* reshape_expr e ltac:(fun K e' => eapply (tac_wp_send _ _ _ _ _ K)). *)
-
-    (* Mod ("Hclose" with "H↦") as "HΦ". done. *)
-
-    (* wp_store. iMod ("Hclose" with "H↦") as "HΦ". done. *)
-
-    (* wp_apply_core wp_send ltac:(fun H => iApplyHyp H) *)
-    (*               idtac. *)
-    (* awp_apply wp_send. *)
-
-
-  (*   {{{ c ↦ M }}} *)
-  (*     send(c, m) *)
-  (*   {{{ RET #(); c ↦ (M ∪ {[m]}) }}}. *)
-  (* Lemma atomic_recv c : *)
-  (*   ⊢ <<< ∀ v, True >>> recv c @ ⊤ <<< True, RET v >>>. *)
-  (* Proof. *)
-
-  Abort.
+    iMod "AU" as "[H↦ [_ Hclose]]".
+    match goal with
+    | |- envs_entails _ (wp ?s ?E ?e ?Q) =>
+        reshape_expr e ltac:(fun K e' => eapply (tac_wp_send _ _ _ _ _ K))
+    end.
+    - iSolveTC.
+    - let l := match goal with |- _ = Some (_, (?l ↦{_} _)%I) => l end in
+        iAssumptionCore.
+    - pm_reduce; first [wp_finish].
+      iMod ("Hclose" with "H↦") as "HΦ". done.
+  Qed.
 
   (* TODO: After defining logical atomic spec to [tryrecv], look at
      [heap_lang.lib.atomic_heap] for atomic heap implementation. *)
